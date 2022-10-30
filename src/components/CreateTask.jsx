979a0732cab851fc27b758feb29aaaad45762504
Kate Svyrydenko <k.m.sviridenko@gmail.com>
2022-10-30 17:58:22 +0200

    Updates

--- src/components/CreateTask.jsx
+++
@@ -1,9 +0,0 @@
-function CreateTask(props) {
-    const { handleFormShow = Function.prototype } = props;
-
-    return <div className="btn-wrap">
-        <div className="waves-effect waves-light btn" onClick={handleFormShow}>Create New Task</div>
-    </div>
-}
-
-export {CreateTask};
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return  <footer className="page-footer">
-                <div className="footer-copyright">
-                    <div className="container"> © 2022 Copyright</div>
-                </div>
-            </footer>
-}
-
-export {Footer};
